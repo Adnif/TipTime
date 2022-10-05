3b7194ca26bb1becc19a9f9c242f3ee691ab11b1
Adnif <ahmadbani9@gmail.com>
2022-10-05 23:52:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tiptime/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.tiptime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tiptime.databinding.ActivityMainBinding;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateTip();
+            }
+        });
+    }
+
+    private void calculateTip(){
+
+        String stringInTextField = binding.costOfService.getText().toString();
+        if(stringInTextField.isEmpty()){
+            stringInTextField = "0";
+        }
+
+        double cost = parseStringToDouble(stringInTextField);
+
+        double tipPercentage;
+        if (binding.tipOptions.getCheckedRadioButtonId() == R.id.option_twenty_percent) {
+            tipPercentage = 0.20;
+        } else if (binding.tipOptions.getCheckedRadioButtonId() == R.id.option_eighteen_percent) {
+            tipPercentage = 0.18;
+        } else {
+            tipPercentage = 0.15;
+        }
+
+        double tip = tipPercentage * cost;
+
+        if(binding.roundUpSwitch.isChecked()){
+            tip = Math.ceil(tip);
+        }
+
+        displayTip(tip);
+    }
+
+
+    private void displayTip(double tip){
+        NumberFormat formattedTip = NumberFormat.getCurrencyInstance(Locale.US);
+        String kntl = getString(R.string.tip_amount,formattedTip.format(tip));
+        binding.tipResult.setText(kntl);
+    }
+
+    private void displayTip2(double tip){
+        NumberFormat formattedTip = NumberFormat.getCurrencyInstance(Locale.US);
+        String kntl = getString(R.string.tip_amount,formattedTip.format(tip));
+        binding.tipResult.setText(kntl);
+    }
+
+    private static double parseStringToDouble(String value) {
+        return value == null || value.isEmpty() ? 0.0 : Double.parseDouble(value);
+    }
+}
